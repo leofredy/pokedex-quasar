c22414f3227e9e02288de38d343cd7156c21d54d
Leonardo Freedy <leonardo.freedy.webart@gmail.com>
2022-04-01 15:57:18 -0300

    Descomenta o header do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pokedexApp">
-    <!-- <Header /> -->
+    <Header />
     <main>
       <router-view v-slot="{ Component }">
         <transition name="fade" mode="out-in">
